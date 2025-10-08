3b3396ed193c2479010072923c43c6beec12a423
MatthiasWentink <matthiaswentink27@gmail.com>
2025-10-08 21:23:30 +0200

    Looser CORS for testing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -55,7 +55,7 @@ function createApp(): void {
   initializeDataSource().then(() => {
     const app = express();
     app.use(cors({
-      origin: true,
+      origin: ['http://localhost:5173', 'https://availabilit.ia.utwente.nl'],
       credentials: true,
     }));
 
