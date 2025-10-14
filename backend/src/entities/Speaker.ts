cd33b384c3ce4b4fa642c2e2a6028545dc396ef2
MatthiasWentink <matthiaswentink27@gmail.com>
2025-10-14 11:09:33 +0200

    Update speaker entity

--- backend/src/entities/Speaker.ts
+++ backend/src/entities/Speaker.ts
@@ -5,9 +5,23 @@ import BaseEnt from './BaseEnt';
 // eslint-disable-next-line import/no-cycle
 import Activity from './Activity';
 
+export interface SpeakerLink {
+  url: string;
+  icon: string;
+}
+
+export enum SpeakerType {
+  KEYNOTE = 'keynote',
+  TALK = 'talk',
+  HOST = 'host',
+}
+
 export interface SpeakerParams {
   name: string;
   description: string;
+  links?: SpeakerLink[];
+  tags?: string[];
+  type: SpeakerType;
 }
 
 @Entity()
@@ -17,6 +31,15 @@ export default class Speaker extends BaseEnt {
 
   @Column({ type: 'text' })
     description: string;
+
+  @Column({ type: 'json', nullable: true })
+    links?: SpeakerLink[];
+
+  @Column({ type: 'json', nullable: true })
+    tags?: string[];
+
+  @Column({ type: 'enum', enum: SpeakerType, default: SpeakerType.TALK })
+    type: SpeakerType;
 
   @ManyToMany(() => Activity, (act) => act.speakers)
     activities: Activity[];
